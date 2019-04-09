e8da935628ad0d235eb2755ae8b960d50a7645a2
Daniele Laudani <danielelaudani@protonmail.com>
2019-04-09 23:09:40 +0200

    removing GPIO A11 usage
    Since I'm not 100% sure that A11 is not connected to anything, I commented it out.

--- main.c
+++ main.c
@@ -19,30 +19,30 @@ void vApplicationStackOverflowHook(xTaskHandle *pxTask,signed portCHAR *pcTaskNa
 
 static void clock_setup(void) {
     //https://github.com/libopencm3/libopencm3-examples/blob/master/examples/stm32/l4/stm32l476g-disco/basics/basics.c
-	rcc_osc_on(RCC_HSI16);
+    rcc_osc_on(RCC_HSI16);
 	
-	flash_prefetch_enable();
-	flash_set_ws(4);
-	flash_dcache_enable();
-	flash_icache_enable();
-	/* 16MHz / 4 = > 4 * 40 = 160MHz VCO => 80MHz main pll  */
-	rcc_set_main_pll(RCC_PLLCFGR_PLLSRC_HSI16, 4, 40,
+    flash_prefetch_enable();
+    flash_set_ws(4);
+    flash_dcache_enable();
+    flash_icache_enable();
+    /* 16MHz / 4 = > 4 * 40 = 160MHz VCO => 80MHz main pll  */
+    rcc_set_main_pll(RCC_PLLCFGR_PLLSRC_HSI16, 4, 40,
 			0, 0, RCC_PLLCFGR_PLLR_DIV2);
-	rcc_osc_on(RCC_PLL);
-	/* either rcc_wait_for_osc_ready() or do other things */
+    rcc_osc_on(RCC_PLL);
+    /* either rcc_wait_for_osc_ready() or do other things */
 
-	/* Enable clocks for the ports we need */
-    rcc_periph_clock_enable(RCC_GPIOA);
+    /* Enable clocks for the ports we need */
+    //rcc_periph_clock_enable(RCC_GPIOA);
 
-	/* Enable clocks for peripherals we need */
-	rcc_periph_clock_enable(RCC_SYSCFG);
+    /* Enable clocks for peripherals we need */
+    rcc_periph_clock_enable(RCC_SYSCFG);
 
-	rcc_set_sysclk_source(RCC_CFGR_SW_PLL); /* careful with the param here! */
-	rcc_wait_for_sysclk_status(RCC_PLL);
-	/* FIXME - eventually handled internally */
-	rcc_ahb_frequency = 80e6;
-	rcc_apb1_frequency = 80e6;
-	rcc_apb2_frequency = 80e6;
+    rcc_set_sysclk_source(RCC_CFGR_SW_PLL); /* careful with the param here! */
+    rcc_wait_for_sysclk_status(RCC_PLL);
+    /* FIXME - eventually handled internally */
+    rcc_ahb_frequency = 80e6;
+    rcc_apb1_frequency = 80e6;
+    rcc_apb2_frequency = 80e6;
 }
 
 static void task1(void *args) {
@@ -50,10 +50,11 @@ static void task1(void *args) {
 
     (void)args;
 
-    gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO11);
+    //not sure if A11 is connected to something
+    //gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO11);
     
     for (;;) {
-        gpio_toggle(GPIOA,GPIO11);
+        //gpio_toggle(GPIOA,GPIO11);
         for (i = 0; i < 300000; i++)
             __asm__("nop");
     }
